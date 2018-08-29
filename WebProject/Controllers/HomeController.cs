62860d9f62e3cc9354e8ccd7c9ace370a3cb14df
chad.stefani <chad.stefani@gmail.com>
2018-08-28 23:28:40 -0400

    contact page email web handler is not set up.

--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ using System.Web;
 using System.Web.Mvc;
 using WebProject.Data;
 using Microsoft.AspNet.Identity;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace WebProject.Controllers
 {
@@ -59,8 +64,47 @@ namespace WebProject.Controllers
             return View();
         }
 
+        ///This is the part that will allow user to send email to us. 
         [HttpPost]
-        public ActionResult Contact(ContactViewModels c)
+        [ValidateAntiForgeryToken]
+       public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("playitfor@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("playitfor@gmail.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                //This part below can be put in the web config file if we want to do that i think.
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "playitfor@gmail.com",  // replace with valid value user@outlook.com
+                        Password = "DeanandChad123!"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+        //99999999999999999999999999999999999999999999999999999999999999999999999999
+        [HttpPost]
+        public ActionResult Contacts(ContactViewModels c)
         {
             ViewBag.Title = "Contact";
 
