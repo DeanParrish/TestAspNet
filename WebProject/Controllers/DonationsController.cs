d39f19bb1f62a7040011f893c8406469d042befe
Dean <parrish.de@gmail.com>
2018-08-31 19:08:56 -0400

    Changed Loan amount to 1, Added logic to only allow donation if user has loan if logged in, if winner and wins with no primary

--- WebProject/Controllers/DonationsController.cs
+++ WebProject/Controllers/DonationsController.cs
@@ -12,16 +12,27 @@ namespace WebProject.Controllers
     public class DonationsController : Controller
     {
         // GET: Donations
-        public ActionResult Index()
+        public ActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(Donations model)
+        public ActionResult Create(Donations model)
         {
             using(DataModel db = new DataModel())
             {
+                var id = User.Identity.GetUserId();
+                if (id != null)
+                {
+                    int userLoanCount = db.Loans.Where(x => x.UserId == id).Count();
+                    if (userLoanCount == 0)
+                    {
+                        TempData["DonationConfirmation"] = "Your donation failed. You must add a loan before you donate!";
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                
                 Loan activeLoan = db.Loans.Where(x => x.isLoanActive == true).FirstOrDefault();
                 List<Donations> donationsTowardLoan = db.Donations.Where(x => x.AppliedLoanId == activeLoan.Id).ToList();
                 double totalDonations = 0;
@@ -32,18 +43,28 @@ namespace WebProject.Controllers
                 }
 
                 //completed loan
-                if ((totalDonations+=model.Amount) >= activeLoan.LoanAmount)
+                if ((totalDonations+=1) >= activeLoan.LoanAmount)
                 {
                     TempData["Winner"] = "Congratulations you're a winner!";
                     activeLoan.isLoanComplete = true;
                     activeLoan.isLoanActive = false;
+                    activeLoan.isLoanPrimary = false;
                     db.SaveChanges();
 
                     var userId = User.Identity.GetUserId();
-                    Loan newActiveLoan = db.Loans.Where(x => x.UserId == userId && x.isLoanActive == false && x.isLoanComplete == false).FirstOrDefault();
+                    Loan newActiveLoan = db.Loans.Where(x => x.UserId == userId && x.isLoanActive == false && x.isLoanComplete == false && x.isLoanPrimary == true).FirstOrDefault();
                     if (newActiveLoan != null)
                     {
-                        newActiveLoan.isLoanActive = true;                        
+                        newActiveLoan.isLoanActive = true;
+                    }
+                    else//no primary
+                    {
+                        TempData["Winner"] = "Congratulations you're a winner! You had no primary loan, so we chose your first loan for you!";
+                        newActiveLoan = db.Loans.Where(x => x.UserId == userId && x.isLoanActive == false && x.isLoanComplete == false).FirstOrDefault();
+                        if (newActiveLoan != null)
+                        {
+                            newActiveLoan.isLoanActive = true;
+                        }
                     }
 
                     db.SaveChanges();
@@ -55,7 +76,7 @@ namespace WebProject.Controllers
 
                 var donation = new Donations()
                 {
-                    Amount = model.Amount,
+                    Amount = 1,
                     ConfirmationId = "Confirmation Static",
                     TimeSubmitted = DateTime.Now,
                     UserId = User.Identity.GetUserId(),
@@ -66,7 +87,6 @@ namespace WebProject.Controllers
                 db.SaveChanges();
 
             }
-            var test = 3;
             
             TempData["DonationConfirmation"] = "Your donation was a success!";
             return RedirectToAction("Index", "Home");
